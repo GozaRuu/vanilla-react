d125c8c25e4dfccb214b9e0160eeef79b665f60b
GozaRuu <kais.sghari@gmail.com>
2018-10-14 01:39:31 +0100

    added and configured webpack with babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env', 'stage-2']
+        }
+      }
+    }]
+  }
+};
