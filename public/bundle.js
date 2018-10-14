9be3b546e5dd91bdfba3f9acfe8c1b2fd5fd9d25
GozaRuu <kais.sghari@gmail.com>
2018-10-14 19:35:44 +0100

    refactored project to seperate render modules in a renderers folder

--- public/bundle.js
+++ public/bundle.js
@@ -134,18 +134,6 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n
 
 /***/ }),
 
-/***/ "./lib/components/index.js":
-/*!*********************************!*\
-  !*** ./lib/components/index.js ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nvar _App = __webpack_require__(/*! ./App */ \"./lib/components/App.js\");\n\nvar _App2 = _interopRequireDefault(_App);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('root'));//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9saWIvY29tcG9uZW50cy9pbmRleC5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9saWIvY29tcG9uZW50cy9pbmRleC5qcz8wNjA3Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7XHJcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20nO1xyXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwJztcclxuXHJcblJlYWN0RE9NLnJlbmRlcihcclxuICA8QXBwIC8+LCBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpXHJcbik7XHJcbiJdLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBOzs7QUFBQTtBQUNBOzs7QUFBQTtBQUNBOzs7OztBQUNBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./lib/components/index.js\n");
-
-/***/ }),
-
 /***/ "./lib/data.json":
 /*!***********************!*\
   !*** ./lib/data.json ***!
@@ -154,6 +142,18 @@ eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/i
 /***/ (function(module) {
 
 eval("module.exports = {\"articles\":[{\"id\":\"95c12a8f6c88953ca8f8a39da25546e6\",\"title\":\"Introducing React's Error Code System\",\"date\":\"Mon Jul 11 2016 00:00:00 GMT+0000 (UTC)\",\"authorId\":\"2c6aa2cfe3449467d329fa17d6ea230f\",\"body\":\"Building a better developer experience has been one of the things that React deeply cares about, and a crucial part of it is to detect anti-patterns/potential errors early and provide helpful error messages when things (may) go wrong. However, most of these only exist in development mode; in production, we avoid having extra expensive assertions and sending down full error messages in order to reduce the number of bytes sent over the wire.\"},{\"id\":\"cc7781c085cf37aabf120098085ff60c\",\"title\":\"Mixins Considered Harmful\",\"date\":\"Wed Jul 13 2016 00:00:00 GMT+0000 (UTC)\",\"authorId\":\"78ae672985c41fae0ecde0133f41bbfa\",\"body\":\"“How do I share the code between several components?” is one of the first questions that people ask when they learn React. Our answer has always been to use component composition for code reuse. You can define a component and use it in several other components. \\nIt is not always obvious how a certain pattern can be solved with composition. React is influenced by functional programming but it came into the field that was dominated by object-oriented libraries. It was hard for engineers both inside and outside of Facebook to give up on the patterns they were used to.\"},{\"id\":\"0a9afe5bb4ecbf4f7f1c77611e9bf1f9\",\"title\":\"Create Apps with No Configuration\",\"date\":\"Fri Jul 22 2016 00:00:00 GMT+0000 (UTC)\",\"authorId\":\"78ae672985c41fae0ecde0133f41bbfa\",\"body\":\"Create React App is a new officially supported way to create single-page React applications. It offers a modern build setup with no configuration. \\n\\nGetting Starte \\nInstallation \\nFirst, install the global package:\"},{\"id\":\"9b72140f27e62670dd7bdd1a9f61b48e\",\"title\":\"Relay: State of the State\",\"date\":\"Fri Aug 05 2016 00:00:00 GMT+0000 (UTC)\",\"authorId\":\"335fb02ec8f76c8515821ac9f266d276\",\"body\":\"This month marks a year since we released Relay and we'd like to share an update on the project and what's next. \\nA Year In Review \\nA year after launch, we're incredibly excited to see an active community forming around Relay and that companies such as Twitter are using Relay in production:\"},{\"id\":\"f4ab6de6e61c86f03f6fef46f7c407f1\",\"title\":\"React v15.5.0\",\"date\":\"Fri Apr 07 2017 00:00:00 GMT+0000 (UTC)\",\"authorId\":\"d85577ea34ae50f2dac5347b5219aa23\",\"body\":\"It's been exactly one year since the last breaking change to React. Our next major release, React 16, will include some exciting improvements, including a complete rewrite of React's internals. We take stability seriously, and are committed to bringing those improvements to all of our users with minimal effort.\\n To that end, today we're releasing React 15.5.0.\"}],\"authors\":[{\"id\":\"d85577ea34ae50f2dac5347b5219aa23\",\"firstName\":\"Andrew\",\"lastName\":\"Clark\",\"website\":\"https://twitter.com/acdlite\"},{\"id\":\"2c6aa2cfe3449467d329fa17d6ea230f\",\"firstName\":\"Keyan\",\"lastName\":\"Zhang\",\"website\":\"https://twitter.com/keyanzhang\"},{\"id\":\"78ae672985c41fae0ecde0133f41bbfa\",\"firstName\":\"Dan\",\"lastName\":\"Abramov\",\"website\":\"https://twitter.com/dan_abramov\"},{\"id\":\"335fb02ec8f76c8515821ac9f266d276\",\"firstName\":\"Joseph\",\"lastName\":\"Savona\",\"website\":\"https://twitter.com/en_JS\"}]};//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9saWIvZGF0YS5qc29uLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./lib/data.json\n");
+
+/***/ }),
+
+/***/ "./lib/renderers/dom.js":
+/*!******************************!*\
+  !*** ./lib/renderers/dom.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nvar _App = __webpack_require__(/*! components/App */ \"./lib/components/App.js\");\n\nvar _App2 = _interopRequireDefault(_App);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('root'));//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9saWIvcmVuZGVyZXJzL2RvbS5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9saWIvcmVuZGVyZXJzL2RvbS5qcz9mYmYzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7XHJcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20nO1xyXG5pbXBvcnQgQXBwIGZyb20gJ2NvbXBvbmVudHMvQXBwJztcclxuXHJcblJlYWN0RE9NLnJlbmRlcihcclxuICA8QXBwIC8+LCBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpXHJcbik7XHJcbiJdLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBOzs7QUFBQTtBQUNBOzs7QUFBQTtBQUNBOzs7OztBQUNBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./lib/renderers/dom.js\n");
 
 /***/ }),
 
@@ -3979,14 +3979,14 @@ eval("var g;\r\n\r\n// This works in non-strict mode\r\ng = (function() {\r\n\tr
 /***/ }),
 
 /***/ 0:
-/*!******************************************************!*\
-  !*** multi babel-polyfill ./lib/components/index.js ***!
-  \******************************************************/
+/*!***************************************************!*\
+  !*** multi babel-polyfill ./lib/renderers/dom.js ***!
+  \***************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! babel-polyfill */"./node_modules/babel-polyfill/lib/index.js");
-module.exports = __webpack_require__(/*! ./lib/components/index.js */"./lib/components/index.js");
+module.exports = __webpack_require__(/*! ./lib/renderers/dom.js */"./lib/renderers/dom.js");
 
 
 /***/ })
